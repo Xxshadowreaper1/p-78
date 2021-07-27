709462f160a42d6462af23a3eea30b3983bb62c1
Xxshadowreaper1 <84533497+Xxshadowreaper1@users.noreply.github.com>
2021-07-27 10:38:05 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-var images = ["","me.JPG","me a mama.JPG","me a mama a tiny.JPG","me a mama a tiny and a papa.PNG", "me bhayia and papa a tincy a mami.PNG", "a.JPG"];
-            var names = ["", "Aarush Rai", "Aarush, and Trupti", "Aarush, Trupti, and Tincy", "Trupti, Tincy, and Raj","Aarush, Trupti, Tincy, Raj, and Yash","Aarush, Trupti, Tincy, Raj, and Arjun" ];
+var images = ["me.JPG",
+"me a mama.JPG",
+"me a mama a tiny.JPG",
+"me a mama a tiny and a papa.PNG",
+ "me bhayia and papa a tincy a mami.PNG",
+  "a.JPG"];
+            var names = ["Aarush Rai", "Aarush, and Trupti", "Aarush, Trupti, and Tincy", "Trupti, Tincy, and Raj","Aarush, Trupti, Tincy, Raj, and Yash","Aarush, Trupti, Tincy, Raj, and Arjun" ];
             var i = 0;
             function update()
             {
                 i++;
-                var numbers_of_family_member_in_array = 6
+                var numbers_of_family_member_in_array = 5;
                 if(i > numbers_of_family_member_in_array )
                 {
                     i = 0;
